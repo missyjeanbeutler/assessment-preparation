3c3e953ef7505ae6b26e946a324bb682b4f2d486
Missy Beutler <missyjeanbeutler@users.noreply.github.com>
2017-02-02 10:01:38 -0700

    final commit for basic

--- basic-js-review/review.js
+++ basic-js-review/review.js
@@ -1,25 +1,46 @@
 /////////////Variables/////////////
 //1. Write a variable called color with a value of "dark blue".
 
+var color = 'dark blue';
+
 //2. Write a variable, myFave, that has the value "My favorite color is "
 
+var myFave = 'My favorite color is';
+
 //3. Use the variables from 1 and 2 to make myFavoriteColor equal
+
+var myFavoriteColor = myFave + " " + color;
+
 //   "My favorite color is dark blue" (concatenate)
 var myFavoriteColor;
 
 //4. Create an array of strings that includes the letters a - d in order.
-var aThroughD;
+var aThroughD = ['a', 'b', 'c', 'd'];
 //5. Remove 'a' and store it to a variable called isForApple.
+var isForApple = aThroughD.shift();
 
 //6. Add 'e' to the end of aThroughD.
+  aThroughD.push('e');
 
 //7. Add 'apple' to the beginning of aThroughD
+aThroughD.unshift('apple');
 
 //8. Remove 'c' from the array and put 'cat' in its place
+aThroughD.splice(2, 1, 'cat');
 
 //9. Leave 'apple' in the array but put a copy of it on a var called bestFruit
 
+var bestFruit = aThroughD[0];
+
 //10. Create an object called spaceJam
+
+var spaceJam = {
+  title: 'Space Jam',
+  release_year: 1996,
+  oscarAwardWinner: false,
+  stars: ['Michael Jordan', 'Bugs Bunny', 'Bill Murray']
+}
+
 //    spaceJam has: a title of 'Space Jam' (String)
 //                  a release_year of 1996 (Number)
 //                  is not an oscarAwardWinner (Boolean)
@@ -36,23 +57,47 @@ var anakinSkywalker =  {
   former_mother: 'Shmi Skywalker'
 }
 
+anakinSkywalker.side = 'dark';
+
 //12. Add 'Jar Jar' to his children using dot notation
+anakinSkywalker.children.push('Jar Jar');
 
 //13. Loop through the object and remove any property that starts with former
 
-
+for(var prop in anakinSkywalker) {
+  var x = prop.split("_");
+  if(x[0] === 'former') {
+    delete anakinSkywalker[prop];
+  }
+}
 
 //14. Create a function called finder that takes in an array and a string and loops through the array
 //    to remove every instance of that string
+
+function finder(arr, str) {
+  for(var i = 0; i < arr.length; i++) {
+    if (arr[i] === str) {
+      arr.splice(arr[i], 1);
+    }
+  }
+}
 
 
 
 //15. Write a function called numChecker that takes in a parameter and checks to see if it is a number or a
   //number in a string (ie '12' or 12) - if so, return true, else return false
-
+function numChecker(a) {
+  return isNaN(a) ? false : true;
+}
 
 
 //16. Write a function called rightNow that returns a new date object
+
+function rightNow() {
+  var str = Date.now()
+  var final = new Date(str);
+  return final;
+}
 
 
 
@@ -60,7 +105,15 @@ var anakinSkywalker =  {
 //    object's name property, and if the object's name
 //    is undefined, return 'has amnesia'.
 
+function whoAmI(obj) {
+  return obj.name === undefined ? 'has amnesia' : obj.name;
+}
+
 
 
 //18. Write a function called returnMe that takes in a num and a callback function and
 //    returns the result of the callback function invoked with the num as a parameter
+
+function returnMe(num, cb) {
+  return cb(num);
+}
